f056e1c839d1b668b322288f880c4f57fa93ca60
Hasnainqari <qarihasnain43@gmail.com>
2026-02-04 18:07:01 +0500

    add preset

--- pages/dashboard/add-blog.js
+++ pages/dashboard/add-blog.js
@@ -75,7 +75,7 @@ export default function AddBlog() {
 
     const formData = new FormData();
     formData.append("file", file);
-    formData.append("upload_preset", "blogimage");
+    formData.append("upload_preset", process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET);
 
     const res = await axios.post(
       `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
